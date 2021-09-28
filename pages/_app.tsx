86d2e752bef59fedc35b77d30f3c32a7e3400d92
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2021-09-28 12:20:23 +0630

    changed:header title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<React.Fragment>
 			<Head>
-				<title>My page</title>
+				<title>Eiga Cloud</title>
 				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 			</Head>
 			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
