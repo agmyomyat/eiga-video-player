2dcebb064113e4d0110882397339ba71626797f6
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2021-10-04 22:52:24 +0630

    added:CORS HEADERS

--- next.config.js
+++ next.config.js
@@ -5,3 +5,16 @@ module.exports = {
 		API_URL: process.env.API_URL,
 	},
 };
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/e/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://eiga.sbs" },
+        ]
+      }
+    ]
+  }
+};
