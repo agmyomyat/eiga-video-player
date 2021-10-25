318fa3ebc28b9b52a61b7a9257f760452b0bb379
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2021-10-25 22:15:50 +0630

    fixed:env error

--- next.config.js
+++ next.config.js
@@ -1,22 +1,35 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	env: {
-		API_URL: process.env.API_URL,
-	},
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/(.*)',
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://google.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          { key: "Access-Control-Expose-Headers", value:"Content-Disposition"}
-        ]
-      }
-    ]
-  }
-};
+   reactStrictMode: true,
+   env: {
+      API_URL: process.env.API_URL,
+      EMBED_URL: process.env.EMBED_URL,
+   },
+   async headers() {
+      return [
+         {
+            // matching all API routes
+            source: '/(.*)',
+            headers: [
+               { key: 'Access-Control-Allow-Credentials', value: 'true' },
+               {
+                  key: 'Access-Control-Allow-Origin',
+                  value: 'https://google.com',
+               },
+               {
+                  key: 'Access-Control-Allow-Methods',
+                  value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+               },
+               {
+                  key: 'Access-Control-Allow-Headers',
+                  value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+               },
+               {
+                  key: 'Access-Control-Expose-Headers',
+                  value: 'Content-Disposition',
+               },
+            ],
+         },
+      ]
+   },
+}
--- pages/e/[id].tsx
+++ pages/e/[id].tsx
@@ -11,50 +11,58 @@ export default function Embed(props:any) {
   const [subLoading,setSubLoading] = useState(true)
   const [fileSize,setFileSize] = useState('')
   useEffect(() => {
-    if(!router.isReady||router.isFallback)return 
-    console.log("router readey",router.isReady)
-    if (router.isReady&& !router.query.token) {
-      console.log("props", router.query)
-      console.log("it fucking")
-      // router.replace('/404')
-      setLoading(false)
-    }
-    embedSubQuery({ eigaLink: (router.query.id as string) }).then((res) => {
-      setSubtitle(res.embedVideos[0].eng_sub)
-      setFileSize(res.embedVideos[0].fileSize)
-      console.log("asdflsadfl",res)
-      setSubLoading(false)
-    }).catch(e => {
-      console.log(e.message)
-      setSubLoading(false)
-    })
-    checkPremiumQuery(router.query.token as string).then((res) => {
-      console.log("result",res)
-      if (res.getUserData.premium) {
-      return setLoading(false)
-      } else {
-      setLoading(false)
-      // router.replace('/404')
-      return
-
-    }
-    }).catch((e) => {
-      console.log(e.message)
-      // router.replace('/404')
-    })
-
-  },[router])
+     if (!router.isReady || router.isFallback) return
+     console.log('router readey', router.isReady)
+     if (router.isReady && !router.query.token) {
+        console.log('props', props)
+        console.log('it fucking')
+        // router.replace('/404')
+        setLoading(false)
+     }
+     embedSubQuery({ eigaLink: router.query.id as string })
+        .then((res) => {
+           setSubtitle(res.embedVideos[0].eng_sub)
+           setFileSize(res.embedVideos[0].fileSize)
+           console.log('asdflsadfl', res)
+           setSubLoading(false)
+        })
+        .catch((e) => {
+           console.log(e.message)
+           setSubLoading(false)
+        })
+     checkPremiumQuery(router.query.token as string)
+        .then((res) => {
+           console.log('result', res)
+           if (res.getUserData.premium) {
+              return setLoading(false)
+           } else {
+              setLoading(false)
+              // router.replace('/404')
+              return
+           }
+        })
+        .catch((e) => {
+           console.log(e.message)
+           // router.replace('/404')
+        })
+  }, [router])
 
   // const jsFileDl= new JsFileDownloader({ url: "https://apidevurn.b-cdn.net/9f797fab-8dab-472a-86ef-d9b8ec5db9f6.mp4", contentType: 'application/octet-stream',autoStart:false })
 
-  if (router.isFallback||loading||subLoading) return <FacebookCircularProgress/>
-	return (
-		<div>
-      <Player fileSize={ fileSize} videoId={(router.query.id as string)} textTrack={subtitle} uuid={props.params.id}/>
-      {/* <Button onClick={download}>download</Button>
+  if (router.isFallback || loading || subLoading)
+     return <FacebookCircularProgress />
+  return (
+     <div>
+        <Player
+           fileSize={fileSize}
+           videoId={router.query.id as string}
+           textTrack={subtitle}
+           uuid={router.query.id as string}
+        />
+        {/* <Button onClick={download}>download</Button>
       <Button onClick={cancelDownload}>Cancel</Button> */}
-		</div>
-	)
+     </div>
+  )
 }
 export async function getStaticPaths() {
   return {
