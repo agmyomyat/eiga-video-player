faa51682c72d98ae13b3d08fd06f14410ef69e33
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-02-20 20:11:31 +0630

    fixed: upload modal transparent

--- src/components/uploadModal.tsx
+++ src/components/uploadModal.tsx
@@ -1,41 +1,40 @@
-import React from "react";
-import { Box, styled } from "@mui/system";
-import Modal from "@mui/material/Modal";
+import React from 'react'
+import { Box, styled } from '@mui/system'
+import Modal from '@mui/material/Modal'
 
 const style = {
-	position: "absolute" as "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 400,
-	bgcolor: "background.paper",
-	border: "2px solid #000",
-	boxShadow: 24,
-	p: 4,
-};
+   position: 'absolute' as 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+   width: 400,
+   border: '2px solid #000',
+   boxShadow: 24,
+   p: 4,
+}
 
-type ModalProps = { open: boolean };
+type ModalProps = { open: boolean }
 
 export const UploadModal = (Children: React.FC) => {
-	// getModalStyle is not a pure function, we roll the style only on the first render
+   // getModalStyle is not a pure function, we roll the style only on the first render
 
-	return function Wrapper({ open }: ModalProps) {
-		const body = (
-			<Box sx={style}>
-				<Children />
-			</Box>
-		);
+   return function Wrapper({ open }: ModalProps) {
+      const body = (
+         <Box sx={style}>
+            <Children />
+         </Box>
+      )
 
-		return (
-			<div>
-				<Modal
-					open={open}
-					aria-labelledby="simple-modal-title"
-					aria-describedby="simple-modal-description"
-				>
-					{body}
-				</Modal>
-			</div>
-		);
-	};
-};
+      return (
+         <div>
+            <Modal
+               open={open}
+               aria-labelledby="simple-modal-title"
+               aria-describedby="simple-modal-description"
+            >
+               {body}
+            </Modal>
+         </div>
+      )
+   }
+}
